b5c9fa40901a399b71becc3a1038b6b78eb2b991
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-13 16:44:38 -0400

    work on editing

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -1,55 +1,69 @@
-import React, { useEffect } from 'react'
-import { Navigate } from 'react-router-dom'
-import PT from 'prop-types'
+import React, { useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import PT from "prop-types";
 
-export default function Articles(props) {
-  // ✨ where are my props? Destructure them here
+export default function Articles({ getArticles, articles, setCurrentArticle }) {
+    // ✨ where are my props? Destructure them here
 
-  // ✨ implement conditional logic: if no token exists
-  // we should render a Navigate to login screen (React Router v.6)
+    // ✨ implement conditional logic: if no token exists
+    // we should render a Navigate to login screen (React Router v.6)
+    const token = localStorage.getItem("token");
+    if (!token) {
+        return <Navigate to="/" />;
+    }
 
-  useEffect(() => {
-    // ✨ grab the articles here, on first render only
-  })
+    useEffect(() => {
+        // ✨ grab the articles here, on first render only
+        (async () => {
+            getArticles();
+        })();
+    }, []);
 
-  return (
-    // ✨ fix the JSX: replace `Function.prototype` with actual functions
-    // and use the articles prop to generate articles
-    <div className="articles">
-      <h2>Articles</h2>
-      {
-        ![].length
-          ? 'No articles yet'
-          : [].map(art => {
-            return (
-              <div className="article" key={art.article_id}>
-                <div>
-                  <h3>{art.title}</h3>
-                  <p>{art.text}</p>
-                  <p>Topic: {art.topic}</p>
-                </div>
-                <div>
-                  <button disabled={true} onClick={Function.prototype}>Edit</button>
-                  <button disabled={true} onClick={Function.prototype}>Delete</button>
-                </div>
-              </div>
-            )
-          })
-      }
-    </div>
-  )
+    return (
+        // ✨ fix the JSX: replace `Function.prototype` with actual functions
+        // and use the articles prop to generate articles
+        <div className="articles">
+            <h2>Articles</h2>
+            {articles?.length === 0
+                ? "No articles yet"
+                : articles.map((art) => {
+                      return (
+                          <div
+                              className="article"
+                              key={art.article_id}>
+                              <div>
+                                  <h3>{art.title}</h3>
+                                  <p>{art.text}</p>
+                                  <p>Topic: {art.topic}</p>
+                              </div>
+                              <div>
+                                  <button onClick={() => setCurrentArticle(art)}>Edit</button>
+                                  <button
+                                      disabled={true}
+                                      onClick={Function.prototype}>
+                                      Delete
+                                  </button>
+                              </div>
+                          </div>
+                      );
+                  })}
+        </div>
+    );
 }
 
 // 🔥 No touchy: Articles expects the following props exactly:
 Articles.propTypes = {
-  articles: PT.arrayOf(PT.shape({ // the array can be empty
-    article_id: PT.number.isRequired,
-    title: PT.string.isRequired,
-    text: PT.string.isRequired,
-    topic: PT.string.isRequired,
-  })).isRequired,
-  getArticles: PT.func.isRequired,
-  deleteArticle: PT.func.isRequired,
-  setCurrentArticleId: PT.func.isRequired,
-  currentArticleId: PT.number, // can be undefined or null
-}
+    articles: PT.arrayOf(
+        PT.shape({
+            // the array can be empty
+            article_id: PT.number.isRequired,
+            title: PT.string.isRequired,
+            text: PT.string.isRequired,
+            topic: PT.string.isRequired,
+        })
+    ).isRequired,
+    getArticles: PT.func.isRequired,
+    deleteArticle: PT.func.isRequired,
+    setCurrentArticle: PT.func.isRequired,
+    currentArticle: PT.object, // can be undefined or null
+};
