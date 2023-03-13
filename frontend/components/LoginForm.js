286c4572018da21bf05da331937c0f30c37f89c6
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-13 16:44:11 -0400

    implement required tasks

--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -1,54 +1,64 @@
-import React, { useState } from 'react'
-import PT from 'prop-types'
+import React, { useState } from "react";
+import PT from "prop-types";
 
 const initialFormValues = {
-  username: '',
-  password: '',
-}
+    username: "",
+    password: "",
+};
 export default function LoginForm(props) {
-  const [values, setValues] = useState(initialFormValues)
-  // ✨ where are my props? Destructure them here
+    const [values, setValues] = useState(initialFormValues);
+    // ✨ where are my props? Destructure them here
 
-  const onChange = evt => {
-    const { id, value } = evt.target
-    setValues({ ...values, [id]: value })
-  }
+    const onChange = (evt) => {
+        const { id, value } = evt.target;
+        setValues({ ...values, [id]: value });
+    };
 
-  const onSubmit = evt => {
-    evt.preventDefault()
-    // ✨ implement
-  }
+    const onSubmit = (evt) => {
+        evt.preventDefault();
+        // ✨ implement
+        props.login(values);
+    };
 
-  const isDisabled = () => {
-    // ✨ implement
-    // Trimmed username must be >= 3, and
-    // trimmed password must be >= 8 for
-    // the button to become enabled
-  }
+    const isDisabled = () => {
+        // ✨ implement
+        // Trimmed username must be >= 3, and
+        // trimmed password must be >= 8 for
+        // the button to become enabled
+        const trimmedUsername = values.username.trim();
+        const trimmedPassword = values.password.trim();
+        return trimmedUsername.length < 3 || trimmedPassword.length < 8;
+    };
 
-  return (
-    <form id="loginForm" onSubmit={onSubmit}>
-      <h2>Login</h2>
-      <input
-        maxLength={20}
-        value={values.username}
-        onChange={onChange}
-        placeholder="Enter username"
-        id="username"
-      />
-      <input
-        maxLength={20}
-        value={values.password}
-        onChange={onChange}
-        placeholder="Enter password"
-        id="password"
-      />
-      <button disabled={isDisabled()} id="submitCredentials">Submit credentials</button>
-    </form>
-  )
+    return (
+        <form
+            id="loginForm"
+            onSubmit={onSubmit}>
+            <h2>Login</h2>
+            <input
+                maxLength={20}
+                value={values.username}
+                onChange={onChange}
+                placeholder="Enter username"
+                id="username"
+            />
+            <input
+                maxLength={20}
+                value={values.password}
+                onChange={onChange}
+                placeholder="Enter password"
+                id="password"
+            />
+            <button
+                disabled={isDisabled()}
+                id="submitCredentials">
+                Submit credentials
+            </button>
+        </form>
+    );
 }
 
 // 🔥 No touchy: LoginForm expects the following props exactly:
 LoginForm.propTypes = {
-  login: PT.func.isRequired,
-}
+    login: PT.func.isRequired,
+};
